cb481a5d86c76294f6fdf1ad3a4e393f88295448
usman-qsols <usmanrahim.qsols@gmail.com>
2023-12-21 19:17:18 +0500

    rerun

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,16 +1,48 @@
-import { Before, After } from "@cucumber/cucumber";
-import { chromium, Browser } from "@playwright/test";
+import {
+  Before,
+  After,
+  BeforeAll,
+  AfterAll,
+  AfterStep,
+  Status,
+} from "@cucumber/cucumber";
+import { chromium, Browser, BrowserContext } from "@playwright/test";
 import { pageFixer } from "./pageFixture";
 
 let browser: Browser;
+let context: BrowserContext;
+
+BeforeAll(async function () {
+  browser = await chromium.launch({ headless: !false });
+});
 
 Before(async function () {
-  browser = await chromium.launch({ headless: false });
+  context = await browser.newContext();
   const page = await browser.newPage();
   pageFixer.page = page;
 });
 
-After(async function () {
+// AfterStep(async function ({ pickle, result }) {
+//   const img = await pageFixer.page.screenshot({ path: `./test-result/screenshots/${pickle.name}.png`, type: "png" })
+//   await this.attach(img, "image/png");
+// });
+
+After(async function ({ pickle, result }) {
+  // screenshot
+
+  if (result?.status == Status.FAILED) {
+    const img = await pageFixer.page.screenshot({
+      path: `./test-results/screenshots/${pickle.name}.png`,
+      type: "png",
+    });
+
+    await this.attach(img, "image/png");
+  }
+
   await pageFixer.page.close();
+  await context.close();
+});
+
+AfterAll(async function () {
   await browser.close();
 });
